9d6f55d13565f67ea8cc367c94632c4969f6f00a
Visal sam <v.sam@telemaque.fr>
2019-10-14 17:26:30 +0200

    mobile responsibe

--- src/components/event.js
+++ src/components/event.js
@@ -1,23 +1,20 @@
 import React from 'react';
-import { Card, Typography } from 'antd';
+import { Card } from 'antd';
 import { Link } from 'react-router-dom';
-import moment from 'moment';
 import EventDate from './eventDate';
 
 const { Meta } = Card;
-const { Text } = Typography;
 
 const Event = (props) => {
   const { id, name, description, user, isModerated, startedDate, endedDate } = props.event;
-  const { imageWidth } = props;
 
   return (
     <div className='event-container'>
       <Link to={`/events/${id}`}>
         <Card
           hoverable
-          style={{ width: imageWidth }}
-          cover={<img height='auto' width='auto' alt="example" src={`https://source.unsplash.com/random/1600x90${id}`}/>}
+          cover={<img height='auto' width='auto' alt="example"
+                      src={`https://source.unsplash.com/random/1600x90${id}`}/>}
         >
           <Meta
             title={`${name} (has been moderated ${isModerated ? 'yes' : 'no'})`}
--- src/main.js
+++ src/main.js
@@ -2,30 +2,23 @@ import React from 'react';
 import Event from './components/event';
 import { events } from './fake/events';
 import './main.css';
-import ButtonWidthManager from './module/buttonWidthManager';
 
 class Main extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       events: events,
-      imageWidth: 800,
     };
   }
-
-  handleWidthChange = (newWidth) => {
-    this.setState({ imageWidth: newWidth });
-  };
 
   render() {
     return (
       <div className='main-container'>
         <div className='pub-1'/>
         <div className="events-container">
-          <ButtonWidthManager handleWidthChange={this.handleWidthChange} />
           {this.state.events.map(event => {
             return (
-              <Event key={event.id} event={event} imageWidth={this.state.imageWidth}/>
+              <Event key={event.id} event={event}/>
             );
           })}
         </div>
